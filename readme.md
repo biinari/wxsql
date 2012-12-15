1b2fba82bd36b216be497ab22ca5b977f74ee12b
Bill Ruddock <bill.ruddock@gmail.com>
2012-12-15 17:42:25 +0000

    initial commit

---
+++ readme.md
@@ -0,0 +1,8 @@
+# wxSQL
+
+A MySQL front end in wxPython.
+
+## Requires
+
+[wxPython](www.wxpython.org)
+[PyMySQL](github.com/petehunt/PyMySQL)
---
+++ wxsql.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import wx
+
+class MainWindow(wx.Frame):
+
+    """ Main Window Frame for wxSQL """
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(610, 550))
+        self.CreateFrames()
+        self.CreateMenu()
+        self.Show(True)
+
+    def CreateFrames(self):
+        grid = wx.GridBagSizer(5, 5)
+        self.SetSizer(grid)
+        self.Centre()
+
+    """ Setup menu bar. """
+    def CreateMenu(self):
+        fileMenu = wx.Menu()
+        menuExit = fileMenu.Append(wx.ID_EXIT, "E&xit", "Quit")
+
+        helpMenu = wx.Menu()
+        menuAbout = helpMenu.Append(wx.ID_ABOUT, "&About", "About wxSQL")
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(fileMenu, "&File")
+        menuBar.Append(helpMenu, "&Help")
+
+        self.SetMenuBar(menuBar)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+
+    def OnAbout(self, event):
+        about = wx.MessageDialog(
+            self,
+            "Work with MySQL databases",
+            "About wxSQL",
+            wx.OK)
+        about.ShowModal()
+        about.Destroy()
+
+    def OnExit(self, event):
+        self.Close(True)
+
+def main():
+    app = wx.App(False)
+    frame = MainWindow(None, "wxSQL")
+    app.MainLoop()
+
+if __name__ == '__main__':
+    main()
