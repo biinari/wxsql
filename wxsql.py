10dc5b252a88f4ea4208a1ac2f0e1cfa44f72760
Bill Ruddock <bill.ruddock@gmail.com>
2012-12-30 23:54:00 +0000

    Variable naming convention

--- wxsql.py
+++ wxsql.py
@@ -17,36 +17,36 @@ class MainWindow(wx.Frame):
         self.Show(True)
 
     def CreateFrames(self):
-        horizSplitter = wx.SplitterWindow(self, style=wx.SP_3D)
-        horizSplitter.SetMinimumPaneSize(20)
-        leftVSplitter = wx.SplitterWindow(horizSplitter, style=wx.SP_3D)
-        leftVSplitter.SetMinimumPaneSize(20)
-        self.databasesTree = DatabasesTree(leftVSplitter, db=self._db,
+        horiz_split = wx.SplitterWindow(self, style=wx.SP_3D)
+        horiz_split.SetMinimumPaneSize(20)
+        left_vert_split = wx.SplitterWindow(horiz_split, style=wx.SP_3D)
+        left_vert_split.SetMinimumPaneSize(20)
+        self.databases_tree = DatabasesTree(left_vert_split, db=self._db,
                 size=(200, 300), style=wx.TR_HIDE_ROOT|wx.TR_DEFAULT_STYLE)
-        self.queryEditor = QueryEditorPanel(horizSplitter)
-        self.display = wx.StaticText(leftVSplitter,
+        self.query_editor = QueryEditorPanel(horiz_split)
+        self.display = wx.StaticText(left_vert_split,
                 label="Select a database above")
 
-        self.databasesTree.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnDBSelChanged)
+        self.databases_tree.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnDBSelChanged)
 
-        leftVSplitter.SplitHorizontally(self.databasesTree, self.display)
-        horizSplitter.SplitVertically(leftVSplitter, self.queryEditor)
+        left_vert_split.SplitHorizontally(self.databases_tree, self.display)
+        horiz_split.SplitVertically(left_vert_split, self.query_editor)
 
     """ Setup menu bar. """
     def CreateMenu(self):
-        fileMenu = wx.Menu()
-        menuExit = fileMenu.Append(wx.ID_EXIT, "E&xit", "Quit")
+        file_menu = wx.Menu()
+        menu_exit = file_menu.Append(wx.ID_EXIT, "E&xit", "Quit")
 
-        helpMenu = wx.Menu()
-        menuAbout = helpMenu.Append(wx.ID_ABOUT, "&About", "About wxSQL")
+        help_menu = wx.Menu()
+        menu_about = help_menu.Append(wx.ID_ABOUT, "&About", "About wxSQL")
 
-        menuBar = wx.MenuBar()
-        menuBar.Append(fileMenu, "&File")
-        menuBar.Append(helpMenu, "&Help")
+        menu_bar = wx.MenuBar()
+        menu_bar.Append(file_menu, "&File")
+        menu_bar.Append(help_menu, "&Help")
 
-        self.SetMenuBar(menuBar)
-        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
-        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+        self.SetMenuBar(menu_bar)
+        self.Bind(wx.EVT_MENU, self.OnExit, menu_exit)
+        self.Bind(wx.EVT_MENU, self.OnAbout, menu_about)
 
     def OnAbout(self, event):
         about = wx.MessageDialog(
@@ -59,8 +59,8 @@ class MainWindow(wx.Frame):
 
     def OnDBSelChanged(self, event):
         item = event.GetItem()
-        text = self.databasesTree.GetItemText(item)
-        if self.databasesTree.IsDatabase(item):
+        text = self.databases_tree.GetItemText(item)
+        if self.databases_tree.IsDatabase(item):
             self._db.selectDatabase(text)
             self.display.SetLabel("db: %s" % text)
         else:
