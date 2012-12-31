486222dad1cf1bff49ecbbbead2ad9c2ef917178
Bill Ruddock <bill.ruddock@gmail.com>
2012-12-31 00:54:47 +0000

    Docstrings

--- app/config.py
+++ app/config.py
@@ -1,6 +1,4 @@
-"""
-Load configuration from config/*.yml
-"""
+""" Load configuration from config/*.yml """
 
 import os
 import yaml
@@ -10,18 +8,21 @@ except ImportError:
     from yaml import Loader, Dumper
 
 class Config (object):
+    """ Configuration settings manager """
 
     _config_dir = None
+    """ Directory containing config files """
 
     __instance = None
 
-    """ Singleton pattern (not thread-safe) """
     def __new__(cls, *args, **kwargs):
+        """ Singleton pattern (not thread-safe) """
         if not cls.__instance:
             cls.__instance = super(Config, cls).__new__(cls, *args, **kwargs)
         return cls.__instance
 
     def __init__(self, config_dir=None):
+        """ Initialise Configuration manager with config directory. """
         super(Config, self).__init__(self)
         if config_dir:
             self._config_dir = config_dir
@@ -29,20 +30,23 @@ class Config (object):
             self._config_dir = os.path.join(os.path.dirname(os.path.dirname(
                 os.path.realpath(__file__))), 'config')
 
-    """ Load config from given yaml file name in config/ """
     def load(self, name):
+        """ Load config from given yaml file name in config directory. """
         file_handle = file(self._get_file_name(name), 'r')
         setattr(self, name, yaml.load(file_handle, Loader=Loader))
 
     def dump(self, name):
+        """ Dump given set of config settings into yaml file. """
         file_handle = file(self._get_file_name(name), 'w')
         yaml.dump(getattr(self, name), file_handle, Dumper=Dumper,
                   default_flow_style=False)
 
     def _get_file_name(self, name):
+        """ Return file path for yaml config with given name. """
         return os.path.join(self._config_dir, '{}.yml'.format(name))
 
     def __getattr__(self, name):
+        """ Automatically load config file if not already loaded. """
         try:
             self.load(name)
             return getattr(self, name)
@@ -52,6 +56,7 @@ class Config (object):
             print e
 
 def main():
+    """ Sanity test for loading database settings. """
     config = Config()
     print config._config_dir
     config.load('database')
--- app/db/mysql.py
+++ app/db/mysql.py
@@ -1,16 +1,22 @@
 import pymysql
 
 class DB(object):
-    """ Database Connection """
+    """ MySQL Database abstraction """
+
     conn = None
+    """ Database Connection """
 
     def __init__(self):
+        """ Connect to database with default settings. """
+        # TODO: Get settings from a named stanza in config
         self.connect(host='localhost', port=3306, user='root', passwd='')
 
     def connect(self, *args, **kwargs):
+        """ Obtain a connection to the database. """
         self.conn = pymysql.connect(*args, **kwargs)
 
     def get_databases(self):
+        """ Return a list of database schema names. """
         cur = self.conn.cursor()
         cur.execute("SHOW DATABASES")
         r = cur.fetchall()
@@ -18,6 +24,7 @@ class DB(object):
         return [i[0] for i in r]
 
     def get_tables(self, database=None):
+        """ Return a list of table names in current or given database. """
         cur = self.conn.cursor()
         if database != None:
             cur.execute("SHOW TABLES FROM `%s`" % database)
@@ -28,17 +35,21 @@ class DB(object):
         return [i[0] for i in r]
 
     def escape(self, obj):
+        """ Escape an object for use in an SQL query. """
         return self.conn.escape(obj)
     
     def select_database(self, database):
+        """ Select a database schema. """
         cur = self.conn.cursor()
         cur.execute("USE `%s`" % database)
         cur.close()
 
     def __del__(self):
+        """ Destructor, close connection. """
         self.conn.close()
 
 def main():
+    """ Print a list of database names for a quick sanity test. """
     db = DB()
     print db.get_databases()
 
