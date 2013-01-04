5da59a5706d61325da34107556f6d2054855de1f
Bill Ruddock <bill.ruddock@gmail.com>
2013-01-04 19:29:36 +0000

    Add Columns and Indexes collections to View

--- app/tree/view.py
+++ app/tree/view.py
@@ -3,8 +3,18 @@ View item in Schema tree.
 """
 
 from item import Item, Container
+from table import ColumnsContainer, IndexesContainer
 
 class View(Item):
     """ View item in Schema tree. """
     
-    pass
+    def expand(self):
+        """ Expand view item with containers. """
+        columns = self.tree.AppendItem(self.item, 'Columns')
+        self.tree.SetItemHasChildren(columns)
+        self.tree.SetPyData(columns, ColumnsContainer(self.tree, columns,
+            'Columns'))
+        indexes = self.tree.AppendItem(self.item, 'Indexes')
+        self.tree.SetItemHasChildren(indexes)
+        self.tree.SetPyData(indexes, IndexesContainer(self.tree, indexes,
+            'Indexes'))
